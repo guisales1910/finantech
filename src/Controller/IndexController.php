cb6fdda1bee47514ddf8d18ec19ce39f0f8cffdc
guilherme.dias <guiguifiel12@gmail.com>
2026-05-29 23:29:32 -0300

    ajuste das rotas

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends AbstractController
+{
+    #[Route('/index', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return new Response("Ola mundo");
+    }
+}
